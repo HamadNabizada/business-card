2e3dd0c2552e2ea20117e678b44a4f04fc55be1e
Hamad <HamadNabizada@gmail.com>
2022-04-06 05:11:33 -0700

    completed JSX and styling for interest section

--- src/Interests.js
+++ src/Interests.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Interests(){
     return(
-        <h1>Interests</h1>
+        <div className="interests-wrapper">
+            <h2>
+                Interests
+            </h2>
+            <p>
+                Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic. 
+            </p>
+        </div>
     )
 }
