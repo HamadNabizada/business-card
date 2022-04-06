0f9520d5de59ba2f7c3165fe45ababe62a2326a1
Hamad <HamadNabizada@gmail.com>
2022-04-06 05:34:20 -0700

    completed JSX and markup for footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Footer(){
     return(
-        <h1>Footer</h1>
+        <div className="footer-wrapper">
+            <a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+            <a href="https://www.facebook.com/"><i class="fa fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a>
+            <a href="https://github.com/"><i class="fa fa-github"></i></a>
+        </div>
     )
 }
